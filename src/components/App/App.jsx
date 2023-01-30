cf537b38b00b29225543680f17ab7f69ae13d540
Kyle Jensen <kjensen19@gmail.com>
2023-01-30 12:37:28 -0600

    added arrow icons for nav

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,15 +27,11 @@ function App(){
 
     return(
         <>
-            <Header />
+            <Header fetchAllBreweries={fetchAllBreweries}/>
             <div className="flex">
                 <BreweryList brewries={brewries} fetchAllBreweries={fetchAllBreweries}/>
             </div>
-            <div className="flex py-4 px-4 gap-4 w-screen justify-center">
-            <button className="px-4 py-1 text-sm max-h-8 shadow-md bg-white text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" onClick={fetchAllBreweries}>All Breweries</button>
-            <button className="px-4 py-1 text-sm max-h-8 shadow-md bg-white text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" onClick={fetchAllBreweries}>Favorites</button>
-            <AddBrewery fetchAllBreweries={fetchAllBreweries} />
-            </div>
+
         </>
     )
 }
--- src/components/BreweryList/BreweryList.jsx
+++ src/components/BreweryList/BreweryList.jsx
@@ -2,13 +2,27 @@ import BreweryItem from "../BreweryItem/BreweryItem"
 
 export default function BreweryList({ brewries, fetchAllBreweries }){
     console.log('breweries: ', brewries)
+    
+    
     return(
-        
-        <div className="border-8 flex flex-wrap gap-2 justify-evenly w-max rounded-xl">
-            {brewries && brewries.map((brewery, i) =>(
-                <BreweryItem brewery={brewery} key={brewery.id} fetchAllBreweries={fetchAllBreweries}/>
-            ))}
-        </div>
+            <div className="border-8 rounded-xl flex flex-row">
+                <button>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-12 h-12 text-white">
+                    <path fillRule="evenodd" d="M13.28 3.97a.75.75 0 010 1.06L6.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0zm6 0a.75.75 0 010 1.06L12.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clipRule="evenodd" />
+                </svg>
+                </button>
+            <div className="flex flex-wrap gap-2 justify-evenly ">
+                {brewries && brewries.map((brewery, i) =>(
+                    <BreweryItem brewery={brewery} key={brewery.id} fetchAllBreweries={fetchAllBreweries}/>
+                ))}
+            </div>
+                <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-12 h-12 text-white">
+                        <path fillRule="evenodd" d="M4.72 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 010-1.06zm6 0a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 010-1.06z" clipRule="evenodd" />
+                    </svg>
+                </button>
+            </div>
+
 
     )
 }
