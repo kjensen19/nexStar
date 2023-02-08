d5fa7c02c34bd381028c51122dae5eb65411cae8
Kyle Jensen <kjensen19@gmail.com>
2023-02-07 21:07:54 -0600

    fixed same issue for going the other way

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,12 +13,17 @@ export default function Header({ fetchBreweries, setCurrentUser, setFavorites, f
 
     }
 
+    const viewAll = () => {
+        setPage(0)
+        setFavorites(false)
+    }
+
     return(
         <div className="flex flex-col w-screen">
             <h1 className='text-4xl text-white font-semibold grid w-screen place-items-center py-2'>{value.length === 0 ? 'MN Breweries' : `${value}'s Breweries`}</h1>
             <div className="flex py-2 px-4 gap-4 w-auto justify-center">
                 <Login setCurrentUser={setCurrentUser} fetchBreweries={fetchBreweries} logoutFunction={logoutFunction}/>
-                <button className="px-4 py-1 text-sm max-h-8 shadow-xl bg-orange-600 text-white font-semibold rounded-full border border-b-4 border-orange-700 hover:text-orange-600 hover:bg-white hover:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2" onClick={() => setFavorites(false)}>All</button>
+                <button className="px-4 py-1 text-sm max-h-8 shadow-xl bg-orange-600 text-white font-semibold rounded-full border border-b-4 border-orange-700 hover:text-orange-600 hover:bg-white hover:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2" onClick={viewAll}>All</button>
                 <button className="px-4 py-1 text-sm max-h-8 shadow-xl bg-orange-600 text-white font-semibold rounded-full border border-b-4 border-orange-700 hover:text-orange-600 hover:bg-white hover:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2" onClick={viewFavorites}>Favorites</button>
                 <AddBrewery fetchBreweries={fetchBreweries} buttonName={'Add'}/>
             </div>
