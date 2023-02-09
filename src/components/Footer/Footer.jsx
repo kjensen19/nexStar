395a3d0b4a1a218c1fef934d77af3dec9eeb4576
Kyle Jensen <kjensen19@gmail.com>
2023-02-08 20:17:42 -0600

    cleaned up currently unused footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,8 @@
-import AddBrewery from "../AddBrewery/AddBrewery"
+
 export default function Footer(){
     return(
         <div>
             <h1>Footer info here</h1>
-            <AddBrewery />
         </div> 
     )
 }
